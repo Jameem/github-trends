5d2b6090ef95ebd553ab1a7b132ace02313f55cd
Jameem <jameemkurikkal.mp@gmail.com>
2024-04-07 08:57:55 +0200

    feat: add filter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Filter } from './components/FIlter';
 import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <main className='container px-1'>
       <Navbar />
+      <Filter />
     </main>
   );
 }
---
+++ src/components/FIlter.tsx
@@ -0,0 +1,9 @@
+export const Filter = () => {
+  return (
+    <section className='filter flex py-1 px-1'>
+      <div>
+        <input type='checkbox' /> Show starred Repositories
+      </div>
+    </section>
+  );
+};
