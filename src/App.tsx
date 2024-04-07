e7c2378ae694090781eb82f22c27dcef9f2f2668
Jameem <jameemkurikkal.mp@gmail.com>
2024-04-07 09:00:55 +0200

    feat: add repositories container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Filter } from './components/FIlter';
 import { Navbar } from './components/Navbar';
+import { Repositories } from './components/repositories/Index';
 
 function App() {
   return (
     <main className='container px-1'>
       <Navbar />
       <Filter />
+      <Repositories />
     </main>
   );
 }
---
+++ src/components/repositories/Index.tsx
@@ -0,0 +1,10 @@
+export const Repositories = () => {
+  return (
+    <section className='py-1 '>
+      <header>
+        <h1>Repositories</h1>
+      </header>
+      {/* TODO: Map repositories here */}
+    </section>
+  );
+};
