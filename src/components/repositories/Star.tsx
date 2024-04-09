470cc99f14c6cfa4e47f2db891a27f01e254454c
Jameem <jameemkurikkal.mp@gmail.com>
2024-04-09 19:25:44 +0200

    feat: add starring feature

--- src/components/repositories/Star.tsx
+++ src/components/repositories/Star.tsx
@@ -1,9 +1,59 @@
-import { FaRegStar } from 'react-icons/fa';
+import { useState } from 'react';
+import { FaRegStar, FaStar } from 'react-icons/fa';
 
-export const Star = () => {
+interface StarProps {
+  id: number;
+  name: string;
+  description?: string;
+  html_url: string;
+  language?: string;
+  stargazers_count?: string;
+}
+
+const isStarredRepo = (repoId: number) => {
+  const starredRepos = localStorage.getItem('starred');
+  if (!starredRepos) {
+    return false;
+  }
+
+  const starredReposArray: StarProps[] = JSON.parse(starredRepos);
+  return starredReposArray.find((repo) => repo.id === repoId);
+};
+
+export const Star = (props: StarProps) => {
+  const [starred, setStarred] = useState(isStarredRepo(props.id));
+
+  const onClick = () => {
+    const starredRepos = localStorage.getItem('starred');
+    if (!starredRepos) {
+      localStorage.setItem('starred', JSON.stringify([props]));
+      setStarred(true);
+      return;
+    }
+
+    const starredReposArray: StarProps[] = JSON.parse(starredRepos);
+    if (!isStarredRepo(props.id)) {
+      localStorage.setItem(
+        'starred',
+        JSON.stringify([...starredReposArray, { ...props }])
+      );
+      setStarred(true);
+      return;
+    }
+
+    const refinedArray = starredReposArray.filter((repo) => {
+      return repo.id !== props.id;
+    });
+    localStorage.setItem('starred', JSON.stringify(refinedArray));
+    setStarred(false);
+  };
   return (
-    <>
-      <FaRegStar color='#c5a455' className='py-1' />
-    </>
+    <div className='repository__header__star'>
+      {starred ? (
+        <FaStar color='#c5a455' onClick={onClick} />
+      ) : (
+        <FaRegStar color='#c5a455' onClick={onClick} />
+      )}
+    </div>
   );
 };
