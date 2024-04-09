dc9cfe4b6bb325a2f0d3d98b9ecfc91fdfed3d4c
Jameem <jameemkurikkal.mp@gmail.com>
2024-04-09 19:28:27 +0200

    feat: add logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,8 @@
 export const Navbar = () => {
-  return <nav className='navbar py-1'>Github Trends</nav>;
+  return (
+    <nav className='navbar py-1'>
+      <img src='logo.svg' width={40} />
+      <span className='navbar__title'>Github Trends</span>
+    </nav>
+  );
 };
