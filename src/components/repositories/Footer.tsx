5a651a04bf966af611aed55a6bcd650d5126f63f
Jameem <jameemkurikkal.mp@gmail.com>
2024-04-09 19:27:47 +0200

    feat: adjust footer styles

--- src/components/repositories/Footer.tsx
+++ src/components/repositories/Footer.tsx
@@ -12,19 +12,33 @@ export const Footer = ({
   stargazers_count,
 }: FooterProps) => {
   return (
-    <div className='repository__footer'>
+    <div className='repository__footer' data-testid='footer'>
       <div className='repository__footer__language-container'>
-        {language && <small>{language}</small>}
+        {language && (
+          <div>
+            <small
+              className='repository__footer__language'
+              data-testid='language'
+            >
+              {language}
+            </small>
+          </div>
+        )}
         <a
           href={githubUrl}
           target='_blank'
           className='repository__footer__link'
+          data-testid='github-link'
         >
           <FaGithub />
           <small>View on Github</small>
         </a>
       </div>
-      {stargazers_count && <small>{stargazers_count} Stars</small>}
+      {stargazers_count && (
+        <small className='repository__footer__stars' data-testid='stars-count'>
+          {stargazers_count} Stars
+        </small>
+      )}
     </div>
   );
 };
