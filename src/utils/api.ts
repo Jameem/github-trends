f9378416d7e2ed036ec673bdc402b39d6f333fd0
Jameem <jameemkurikkal.mp@gmail.com>
2024-04-10 16:38:07 +0200

    chore: adjust language filter in the api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -10,11 +10,8 @@ export const initializeAxios = () => {
   axios.defaults.headers.common['Content-Type'] = 'application/json';
 };
 
-export const getRepos = async (page: number, language?: string) => {
-  const url = `repositories?q=created:>${getDate()}&sort=stars&order=desc&per_page=10&page=${page}`;
+export const getRepos = async (page: number, language = '') => {
+  const url = `repositories?q=created:>${getDate()}+language:${language}&sort=stars&order=desc&per_page=10&page=${page}`;
 
-  if (!language) {
-    return axios.get(url);
-  }
-  return axios.get(`${url}&q=language:${language}`);
+  return axios.get(url);
 };
